128aa3b43b1b661b264182f7982d67f2110337b1
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-06-15 21:29:42 -0300

    up

---
+++ fundamentos/inicio/bloco.js
@@ -0,0 +1,18 @@
+//bloco em javascript é definido por base de chaves {}
+{
+    console.log("Passo #01");
+    console.log("Passo #02")
+    console.log("Passo #03")
+}
+
+{
+    console.log('Bloco de Código')
+}
+
+{
+    ;
+    ;
+    ;
+}
+
+console.log("Fim")
