2616dedfc010eccaab0384edac0d323604ead298
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-06-15 21:39:44 -0300

    up

---
+++ fundamentos/inicio/dados.js
@@ -0,0 +1,12 @@
+var nome = "caneta";
+var quantidade = 10;
+var preco = 6.4;
+let imposto = 1.5;
+let precoFinal = preco + imposto; // toda vez que for referênciar uma variavel com dois nomes, o segundo ou terceiro nome deve ser em maiuscula a primeira letra
+
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
