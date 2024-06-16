3d9707de5eb4f370d5211c2a1ab7d91f2918b6ad
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-06-15 21:50:46 -0300

    up

---
+++ fundamentos/inicio/dados2.js
@@ -0,0 +1,14 @@
+console.log(19.9 * 0.6); // valor literal
+
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto); //variavel
+
+console.log(precoComDesconto);
+
+let nome = "caderno";
+let categoria = "papelaria";
+console.log("Produto: " + nome 
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto);
